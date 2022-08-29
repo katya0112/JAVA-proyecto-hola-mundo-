ca065686593997f69453833f481be79906f70a8b
katya0112 <hdzkatya07@gmail.com>
2022-08-29 07:44:16 -0600

    primer commit

---
+++ src/main/java/com/mycompany/holamundo/Holamundo.java
@@ -0,0 +1,15 @@
+/*licencias de uso*/
+
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author shoto
+ */
+public class Holamundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
